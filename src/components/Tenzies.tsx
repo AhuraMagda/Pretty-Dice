a727955166fd3505b029a9a0a110ed7462cf4782
AhuraMagda <104498979+AhuraMagda@users.noreply.github.com>
2023-08-31 19:16:40 +0200

    update tenzies

--- src/components/Tenzies.tsx
+++ src/components/Tenzies.tsx
@@ -1,10 +1,13 @@
 import D6 from "./D6";
-import React from "react";
+import React, { ChangeEvent } from "react";
+import "./Tenzies.css"
+
 
 function Tenzies() {
 
     const [sides, setSides] = React.useState(["0","1","2","3","4","5","6","7"])
 
+    const [clickCount, setClickCount] = React.useState(0)
 
     const handleSideChange = (newSide: string, id: number) => {
         setSides(prevSides => prevSides.map((side, index) => index === id ? newSide : side))
@@ -19,6 +22,7 @@ function Tenzies() {
       }
     
     const allTheDice  = makeNewDice()
+    
     const [tenzies, setTenzies] = React.useState(false) 
 
 
@@ -36,16 +40,51 @@ function Tenzies() {
         diceToClick.forEach(die => (die as HTMLElement).click())
         setTenzies(false)
     }
+    
+    const [playersOnTheBoard, setPlayersOnTheBoard] = React.useState([<p>Magda 31.08.2023</p>])
 
+    const [playerName, setPlayerName] = React.useState("")
+
+    const date = new Date();
+    let currentDay= String(date.getDate()).padStart(2, '0');
+    let currentMonth = String(date.getMonth()+1).padStart(2,"0");
+    let currentYear = date.getFullYear();
+    let currentDate = ` ${currentDay}.${currentMonth}.${currentYear}`;
+
+    const [isPlayerOnTheBoard, setIsPlayerOnTheBoard] = React.useState(false)
+
+    const writeNewPlayer = () => {
+        setPlayersOnTheBoard(prevPlayers => [...prevPlayers, <p>{playerName}{currentDate}</p>])
+        setIsPlayerOnTheBoard(true)
+    }
+
+    const handlePlayerName = (event: ChangeEvent<HTMLInputElement>) => {
+        const { value } = event.target       
+        setPlayerName(value)
+    }
+
+    // Players Board
     return (
         <div className="game-container tenzies">
             <h1>Tenzies</h1>
             {!tenzies && <p>Click on the die untill all the dies are the same</p>}
             {tenzies && <h2>TENZIES! You Won</h2>}
+            <h2>Your moves: {clickCount}</h2>
             <div className="dice-container">
                 {allTheDice}
             </div>
             {tenzies && <button className="tenzies-new" onClick={newGame}>New Game</button>}
+
+            
+            {tenzies && <div className="players-board">
+                {!isPlayerOnTheBoard && <div>
+                    <label htmlFor="name">Name: </label>
+                    <input type="text" id="name" value={playerName} onChange={handlePlayerName} />
+                    <button id="players-board__button" onClick={writeNewPlayer}>save</button>
+                </div>}
+                <h2>Users that played:</h2>
+                {playersOnTheBoard}
+            </div>}
         </div>
     )
 }
