6ce703f28521f946ff8393174a4d04d97412a214
AhuraMagda <lubaszka.magda@gmail.com>
2023-10-26 14:38:14 +0200

    active link

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,24 @@
 import './App.css'
 import RollDice from './components/RollDice'
 import Tenzies from './components/Tenzies'
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom"
 import Footer from './components/Footer'
 
 function App() {
+
+const activeStyle = {
+  textDecoration: "underline"
+}
+
+const customStyle = ({ isActive }: { isActive: boolean }) =>
+isActive ? activeStyle : null;
 
   return (
     <>
       <main>
         <BrowserRouter>
-        <Link to="/">Roll Dice</Link>
-        <Link to="/tenzies">Tenzies</Link>
+        <NavLink style={customStyle} to="/">Roll Dice</NavLink>
+        <NavLink style={customStyle} to="/tenzies">Tenzies</NavLink>
           <Routes>
             <Route path="/" element={<RollDice />} />
             <Route path="/tenzies" element={<Tenzies />} />
