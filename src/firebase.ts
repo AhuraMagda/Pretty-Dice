59d79ae03061f8ed20c16f74417c7717a013108a
AhuraMagda <lubaszka.magda@gmail.com>
2023-11-05 10:18:12 +0100

    env try

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,27 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection } from "firebase/firestore";
 import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID, MEASUREMENT_ID } from "./utils/env";
 
-console.log(API_KEY)
-
-// const firebaseConfig = {
-//   apiKey: API_KEY,
-//   authDomain: AUTH_DOMAIN,
-//   projectId: PROJECT_ID,
-//   storageBucket: STORAGE_BUCKET,
-//   messagingSenderId: MESSAGING_SENDER_ID,
-//   appId: APP_ID,
-//   measurementId: MEASUREMENT_ID,
-// };
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD65XuzxMTjcWD_EHuiM7nL6Ll-q4_kbcI",
-  authDomain: "tenzies-5784f.firebaseapp.com",
-  projectId: "tenzies-5784f",
-  storageBucket: "tenzies-5784f.appspot.com",
-  messagingSenderId: "248968153964",
-  appId: "1:248968153964:web:8f97cc7d9839b2b1ad8149",
-  measurementId: "G-VCM1EDR773"
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
 };
+
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
